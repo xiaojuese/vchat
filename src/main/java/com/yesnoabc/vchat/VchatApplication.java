f6e21d696dc0a29cdb8cde75db77f2a349573eaa
qupeng03 <qupeng03@meituan.com>
2023-03-26 10:51:09 +0800

    Initial commit

---
+++ src/main/java/com/yesnoabc/vchat/VchatApplication.java
@@ -0,0 +1,13 @@
+package com.yesnoabc.vchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VchatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VchatApplication.class, args);
+    }
+
+}
